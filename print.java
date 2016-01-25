c8c988108bfd46d39c33b21d4928f8c943332b4f
Marcin Ciesielczyk <ciesielczyk5@gmail.com>
2016-01-25 15:42:51 +0100

    dodanie czwartego

--- print.java
+++ print.java
@@ -3,7 +3,7 @@
  */
 public class print {
     public static void main(String[] args){
-        System.out.println("Napis brancha drugiego");
+        System.out.println("Napis brancha czwartego");
 
     }
 }
